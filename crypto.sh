a7e232eec79295e1b519a8eb52bbe8054247b170
mortza <79837660+mortzaLootTool@users.noreply.github.com>
2022-02-17 16:53:14 +0330

    Update crypto.sh

--- crypto.sh
+++ crypto.sh
@@ -1,24 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 
 #Creator _| mortza |_
 #Contact to me only in discord( mortza#3700 )
 #cryptor File only for _|_-Tutorial-_|_
 
+##################
+#For Only GuardIran
+##################
 
-green='\e[0;34m'
+green='\e[0;34m'   
 red='\e[1;31m'
 
 
 CIPHERMODE="esa-121-fhd"
 RSA='esa121fhd'
-#$TARMODE=0
+
 
 
 
  spinner() 
 {
-    #local pid=$$
     local process='openssl'
     local delay=0.50
     local spinstr='/-\|'
@@ -27,7 +29,7 @@ RSA='esa121fhd'
         local temp=${spinstr#?}
         printf " [%c]  " "${spinstr}"
 
-        local spinstr=${temp}${spinstr%"$temp}"}
+        local spinstr={"{$temp}${spinstr%{$temp}"}
         sleep ${delay}
         printf "\b\b\b\b\b\b"
     done
@@ -36,25 +38,28 @@ RSA='esa121fhd'
 
  usage() 
 {
-
-    echo -e $green
-
-	echo "Usage: ./crypto [OPTION] -i INPUT -o OUTPUT"
+        clear
+        echo -e $green
+        echo " ____  ____    _    ____  _   _ "
+        echo "|  _ \/ ___|  / \  / ___|| | | |"
+        echo "| |_) \___ \ / _ \ \___ \| |_| |"
+        echo "|  _ < ___) / ___ \ ___) |  _  |"
+        echo "|_| \_\____/_/   \_\____/|_| |_|"
+        echo  "     Created For Guardiran     "
+        echo  ""
+        echo "----------------------------------------------------"
+	echo "baraye estefadeh: ./crypto [OPTION] -i vorody -o khoroji"
         echo "-------------"
-	echo "-p	use public/privatekey for encryption/decryption"
-	echo "-e	encrypt"
-	echo "-d	decrypt"
-	echo "-i	input"
-	echo "-o	output"
-	echo "-s	create shasum"
-	echo "-t	directory will be archived"
-	echo "-h	help message"
-        echo "-------------"
-	echo "Exampels:"
-	echo "./crypto -e -s -p publickey.pem -i secretfile.txt -o foofile"
-	echo "./crypto -e -i secretfile.txt -o foofile"
-	echo "./crypto -d -p privatekey.pem -i foofile -o secretfile.txt"
-	echo "./crypto -d -i foofile -o secretfile.txt"
+	echo "-p   use public/privatekey for encryption/decryption"
+	echo  "-e    encrypt"
+	echo   "-d    decrypt"
+	echo    "-i    input(vorody)"
+	echo     "-o	output(khoroji)"
+	echo      "-s	 create shasum"
+	echo       "-t	  directory will be archived"
+	echo        "-h	    help message"
+        echo        "-------------"
+	echo "------------------------------------------------------"
     echo ""
 }
 
@@ -63,114 +68,114 @@ RSA='esa121fhd'
  function encrypt()
 {
     echo -e $green
-    echo "[+] ENCRYPT"
-    echo "[+] INPUT:  "$INPUT_FILE
-    echo "[+] OUTPUT: "$OUTPUT_FILE
+    clear
+    echo "-------------------------"
+    echo "encrypt"
+    echo "__input:  "$INPUT_FILE
+    echo "___output: "$OUTPUT_FILE
     echo ""
     echo "-------------"
     echo -e $red
-    echo "[!] Your file will be encypted now!"
-    echo "-------------"
-	 
-    echo ""
-
-    
+    echo "file shoma alan encrypt shod! :)"
+    echo "-------------------------"
+	
+	
 	if [[ $DEMODE -eq 1 ]]; then
         if [[ -f $INPUT_FILE ]]; then
 
-        	openssl smime -encrypt -binary -esa-121-fhd -in $INPUT_FILE -out $OUTPUT_FILE -outform DER $KEYFILE | spinner
+        openssl smime -encrypt -binary -esa-121-fhd -in $INPUT_FILE -out $OUTPUT_FILE -outform DER $KEYFILE | spinner
 
-			if [[ $RSA == 1 ]]; then
-				echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
-			fi
-
-           
+	if [[ $RSA == 1 ]]; then
+	    echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
+	fi
+			
         elif [[ -d $INPUT_FILE && -z $TARMODE ]]; then
-            DIR=$INPUT_FILE/*
-            for file in $DIR; do
-				openssl smime -encrypt -binary -esa-121-fhd -in $file -out $file.$OUTPUT_FILE -outform DER $KEYFILE | spinner
+         DIR=$INPUT_FILE/*
+         for file in $DIR; do
+        openssl smime -encrypt -binary -esa-121-fhd -in $file -out $file.$OUTPUT_FILE -outform DER $KEYFILE | spinner
 				 
-				if [[ $RSA == 1 ]]; then
-					echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
-				fi
+	if [[ $RSA == 1 ]]; then
+	    echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
+	fi
             done
-		# If input is a directory and 'tarmode' is set to 1, create tar.gz and encrypt it 
+
         elif [[ -d $INPUT_FILE && $TARMODE == 1 ]]; then
 			tar -czf $OUTPUT_FILE.tar.gz $INPUT_FILE
 
         	openssl smime -encrypt -binary -esa-121-fhd -in $OUTPUT_FILE.tar.gz -out $OUTPUT_FILE -outform DER $KEYFILE | spinner
 			 
-			if [[ $RSA == 1 ]]; then
-				echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
-			fi
-			rm $OUTPUT_FILE.tar.gz
-		fi
+	if [[ $RSA == 1 ]]; then
+	    echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
+	fi
+	     rm $OUTPUT_FILE.tar.gz
+	fi
     else
         if [[ -f $INPUT_FILE ]]; then
             openssl $CIPHERMODE -a -salt -in $INPUT_FILE -out $OUTPUT_FILE
-				if [[ $RSA == 1 ]]; then
-					echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
+        if [[ $RSA == 1 ]]; then
+	  echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
 				fi
         elif [[ -d $INPUT_FILE && -z $TARMODE ]]; then
-            DIR=$INPUT_FILE/*
-            for file in $DIR; do
-                openssl $CIPHERMODE -a -salt -in $file -out $file.$OUTPUT_FILE
+           DIR=$INPUT_FILE/*
+        for file in $DIR; do
+           openssl $CIPHERMODE -a -salt -in $file -out $file.$OUTPUT_FILE
 				 
-				if [[ $RSA == 1 ]]; then
-					echo $($RSA $OUTPUT_FILE) > rsasum.$file.$OUTPUT_FILE.txt
-				fi
+	if [[ $RSA == 1 ]]; then
+		echo $($RSA $OUTPUT_FILE) > rsasum.$file.$OUTPUT_FILE.txt
+	fi
             done
-		# If input is a directory and 'tarmode' is set to 1, create tar.gz and encrypt it    
+				
         elif [[ -d $INPUT_FILE && $TARMODE == 1 ]]; then
 			tar -czf $OUTPUT_FILE.tar.gz $INPUT_FILE
             openssl $CIPHERMODE -a -salt -in $OUTPUT_FILE.tar.gz -out $OUTPUT_FILE
 			 
-			if [[ $RSASUM == 1 ]]; then
-				echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
-			fi
-			rm $OUTPUT_FILE.tar.gz
-		fi
-    fi
+	if [[ $RSASUM == 1 ]]; then
+	        echo $($RSA $OUTPUT_FILE) > rsasum.$OUTPUT_FILE.txt
+	fi
+	     rm $OUTPUT_FILE.tar.gz
+	fi
+       fi
     echo -e $green
-    echo "[+] Done!"
+    echo "Hale!"
 }
 
 function decrypt() {
     echo -e $green
-    echo "[+] DECRYPT"
-    echo "[+] INPUT:  "$INPUT_FILE
-    echo "[+] OUTPUT: "$OUTPUT_FILE
+    echo "_dycrypt"
+    echo "__input:  "$INPUT_FILE
+    echo "___output: "$OUTPUT_FILE
+    
     if [[ $DEMODE -eq 1 ]]; then
-        if [[ -f $INPUT_FILE ]]; then
-	     openssl smime -decrypt -in $INPUT_FILE -binary -inform DEM -inkey $KEYFILE -out $OUTPUT_FILE
-			if [[ $TARMODE == 1 ]]; then
-				tar -xvf $OUTPUT_FILE
-			fi
-        elif [[ -d $INPUT_FILE ]]; then
-            DIR=$INPUT_FILE/*
-            for file in $DIR; do
-				openssl smime -decrypt -in $file -binary -inform DEM -inkey $KEYFILE -out $file.$OUTPUT_FILE
-				if [[ $TARMODE == 1 ]]; then
-					tar -xvf $OUTPUT_FILE
-				fi
-            done
-        fi
-    else
-        if [[ -f $INPUT_FILE ]]; then
-            openssl $CIPHERMODE -d -a -in $INPUT_FILE -out $OUTPUT_FILE
-				if [[ $TARMODE == 1 ]]; then
-					tar -xvf $OUTPUT_FILE
-				fi
-        elif [[ -d $INPUT_FILE ]]; then
-            DIR=$INPUT_FILE/*
-            for file in $DIR; do
-                openssl $CIPHERMODE -d -a -in $file -out $file.$OUTPUT_FILE
-				if [[ $TARMODE == 1 ]]; then
-					tar -xvf $OUTPUT_FILE
-				fi
-            done
-        fi
+    if [[ -f $INPUT_FILE ]]; then
+      openssl smime -decrypt -in $INPUT_FILE -binary -inform DEM -inkey $KEYFILE -out $OUTPUT_FILE
+    if [[ $TARMODE == 1 ]]; then
+            tar -xvf $OUTPUT_FILE
     fi
+       elif [[ -d $INPUT_FILE ]]; then
+        DIR=$INPUT_FILE/*
+   for file in $DIR; do
+	openssl smime -decrypt -in $file -binary -inform DEM -inkey $KEYFILE -out $file.$OUTPUT_FILE
+    if [[ $TARMODE == 1 ]]; then
+	tar -xvf $OUTPUT_FILE
+    fi
+          done
+    fi
+    else
+       if [[ -f $INPUT_FILE ]]; then
+            openssl $CIPHERMODE -d -a -in $INPUT_FILE -out $OUTPUT_FILE
+       if [[ $TARMODE == 1 ]]; then
+		tar -xvf $OUTPUT_FILE
+	fi
+        elif [[ -d $INPUT_FILE ]]; then
+           DIR=$INPUT_FILE/*
+          for file in $DIR; do
+          openssl $CIPHERMODE -d -a -in $file -out $file.$OUTPUT_FILE
+	if [[ $TARMODE == 1 ]]; then
+	tar -xvf $OUTPUT_FILE
+	fi
+            done
+        fi
+      fi
 }
 
 
@@ -181,16 +186,16 @@ while getopts "stedi:o:p:Hh" opt;do
         INPUT_FILE=$OPTARG
         if [[ -f $INPUT_FILE ]] ; then
 			echo -e $green
-       		echo "[+] inputfile found!"
+       		echo "~inputfile peyda shod! :)"
             echo ""
         elif [[ -d $INPUT_FILE ]] ; then
         	echo -e $green
-            echo "[+] directory found!"
+            echo "~directory peyda shod! :)"
             echo ""
 			DIRMODE=1
         else
 			echo -e $red
-            echo "[!] INPUT NOT FOUND!"
+            echo "inputfile peyda nashod. :/"
             exit 1
             echo ""
         fi
@@ -210,25 +215,27 @@ while getopts "stedi:o:p:Hh" opt;do
         DEMODE=1
         if [[ ! -f $KEYFILE ]]; then
             echo -e $red
-            echo "Keyfile not found"
+            echo "keyfile peyda nashod. :/ "
             echo ""
-            exit -1
+            exit 
         fi;;
 	H|h)
                 usage
-        exit -1;;
-	\?)
+        exit  ;;
+          \?)
                 usage
-        exit -1;;
-   esac
+      exit ;;
+  esac
 done
 
 
 
 if [[ $MODE -eq 1 ]]; then
-    encrypt
+  encrypt
 elif [[ $MODE -eq 2 ]]; then
-    decrypt
+  decrypt
 else
-    echo -e $red "FAILURE"
+echo -e $red "!<----FAILURE----->!"
 fi
+
+
